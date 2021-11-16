a32bb0483d5cd68b4f2ce6fc80b16b433ff2fb71
Adam Rybinski <adam@rybinski.info>
2021-11-16 23:44:42 +0100

    Add Hero to Homepage

---
+++ src/Pages/Home/Hero.tsx
@@ -0,0 +1,20 @@
+const Hero = () => (
+    <div class="bg-cover bg-center h-screen w-screen" style="background-image: url('/src/assets/hero.png')">
+        <div class="h-48"></div>
+        <h1 class="text-white text-center text-6xl font-bold">
+            Deriv API
+        </h1>
+        <div class="h-4"></div>
+        <div class="flex flex-row justify-center">
+            <div class="w-1/3"></div>
+            <div class="text-white text-2xl text-center">
+                Use our powerful, flexible,
+                and free API to build a custom trading platform
+                – for yourself or for your business.
+            </div>
+            <div class="w-1/3"></div>
+        </div>
+    </div>
+);
+
+export default Hero;
